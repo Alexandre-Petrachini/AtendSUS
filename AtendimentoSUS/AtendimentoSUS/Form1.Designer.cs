cd954f99b5c8030ac61bc032dcf5805609f6ad43
Alexandre Petrachini <36488039+Alexandre-Petrachini@users.noreply.github.com>
2018-02-14 19:24:18 -0200

    Add files via upload

---
+++ AtendimentoSUS/AtendimentoSUS/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace FilaEspera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ChamarSenha = new System.Windows.Forms.Button();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.Senhas = new System.Windows.Forms.ListBox();
+            this.lbTexto = new System.Windows.Forms.Label();
+            this.Paciente = new System.Windows.Forms.Label();
+            this.lbHora = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Senha = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ChamarSenha
+            // 
+            this.ChamarSenha.Location = new System.Drawing.Point(694, 35);
+            this.ChamarSenha.Name = "ChamarSenha";
+            this.ChamarSenha.Size = new System.Drawing.Size(90, 23);
+            this.ChamarSenha.TabIndex = 1;
+            this.ChamarSenha.Text = "Chamar Senha";
+            this.ChamarSenha.UseVisualStyleBackColor = true;
+            this.ChamarSenha.Click += new System.EventHandler(this.btnChamarSenha_Click);
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.ForeColor = System.Drawing.Color.White;
+            this.lblSenha.Location = new System.Drawing.Point(12, 357);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(128, 96);
+            this.lblSenha.TabIndex = 3;
+            this.lblSenha.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblSenha.Visible = false;
+            // 
+            // Senhas
+            // 
+            this.Senhas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Senhas.FormattingEnabled = true;
+            this.Senhas.ItemHeight = 24;
+            this.Senhas.Location = new System.Drawing.Point(694, 81);
+            this.Senhas.Name = "Senhas";
+            this.Senhas.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Senhas.Size = new System.Drawing.Size(86, 196);
+            this.Senhas.TabIndex = 6;
+            // 
+            // lbTexto
+            // 
+            this.lbTexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTexto.Location = new System.Drawing.Point(100, 246);
+            this.lbTexto.Name = "lbTexto";
+            this.lbTexto.Size = new System.Drawing.Size(512, 82);
+            this.lbTexto.TabIndex = 5;
+            this.lbTexto.Text = "Favor dirigir-se à consulta";
+            this.lbTexto.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Paciente
+            // 
+            this.Paciente.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Paciente.ForeColor = System.Drawing.Color.Gold;
+            this.Paciente.Location = new System.Drawing.Point(546, 554);
+            this.Paciente.Name = "Paciente";
+            this.Paciente.Size = new System.Drawing.Size(251, 48);
+            this.Paciente.TabIndex = 7;
+            this.Paciente.Text = "Data";
+            this.Paciente.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbHora
+            // 
+            this.lbHora.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbHora.ForeColor = System.Drawing.Color.Chocolate;
+            this.lbHora.Location = new System.Drawing.Point(4, 542);
+            this.lbHora.Name = "lbHora";
+            this.lbHora.Size = new System.Drawing.Size(212, 66);
+            this.lbHora.TabIndex = 8;
+            this.lbHora.Text = "Horario";
+            this.lbHora.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FilaEspera.Properties.Resources.sus11;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(310, 231);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Senha
+            // 
+            this.Senha.AutoSize = true;
+            this.Senha.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Senha.Location = new System.Drawing.Point(307, 328);
+            this.Senha.Name = "Senha";
+            this.Senha.Size = new System.Drawing.Size(0, 55);
+            this.Senha.TabIndex = 9;
+            this.Senha.Click += new System.EventHandler(this.Senha_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(809, 611);
+            this.Controls.Add(this.Senha);
+            this.Controls.Add(this.lbTexto);
+            this.Controls.Add(this.lbHora);
+            this.Controls.Add(this.Paciente);
+            this.Controls.Add(this.Senhas);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ChamarSenha);
+            this.Controls.Add(this.lblSenha);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Controle de Senhas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.Resize += new System.EventHandler(this.Form1_Resize_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ChamarSenha;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox Senhas;
+        private System.Windows.Forms.Label lbTexto;
+        private System.Windows.Forms.Label Paciente;
+        private System.Windows.Forms.Label lbHora;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label Senha;
+    }
+}
+
---
+++ AtendimentoSUS/AtendimentoSUS/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Media;
+using AtendimentoSUS;
+using System.Globalization;
+
+namespace FilaEspera
+{
+    public partial class Form1 : Form
+    {
+        string tipo = " ";
+        int maxSenha = 0;
+
+      
+   
+   
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChamarSenha_Click(object sender, EventArgs e)
+        {
+            lblSenha.Visible = true;
+
+            tipo = "P";
+
+            pegaSenha();
+
+
+            if (maxSenha > 0)
+            {
+                lblSenha.Text = tipo + maxSenha.ToString();
+            }
+            else
+            {
+                tipo = "N";
+                
+
+                pegaSenha();
+
+                if (maxSenha > 0)
+                {
+                    lblSenha.Text = tipo + maxSenha.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Não há pacientes no momento.");
+                    return;
+                }
+            }
+
+
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            synth.SetOutputToDefaultAudioDevice();
+            synth.Volume = 100;
+
+            SystemSounds.Beep.Play();
+            synth.Speak(lblSenha.Text);
+
+            Senhas.Items.Add("  " + lblSenha.Text);
+
+            if (Senhas.Items.Count != 0)
+            {
+                Senhas.SelectedIndex = Senhas.Items.Count - 1;
+            }
+
+            gravaChamada();
+
+        }
+
+        private void pegaSenha()
+        {
+            Conexao comb = new Conexao();
+            String status = "";
+            comb.open();
+
+            comb.sql = "Select min(tb01_seq) from tb01_senhas ";
+            comb.sql += " where tb01_tipo =  '"+ tipo +"'";
+            comb.sql += " and   tb01_status= '" + status + "'";
+
+            
+          
+
+            maxSenha = Convert.ToInt32(comb.ExecuteScalar());
+
+            if (maxSenha == 0)
+            {
+                Senha.Text = "";
+            }
+            else
+            {
+                Senha.Text = "" + tipo + "" + maxSenha;
+            }
+
+            comb.close();
+        }
+
+        private void gravaChamada()
+        {
+            Conexao comb = new Conexao();
+
+            comb.open();
+
+            comb.sql = "Update tb01_senhas set ";
+            comb.sql += " tb01_status = 's'";
+            comb.sql += " where tb01_tipo = '" + tipo + "'";
+            comb.sql += " and   tb01_seq = '" + maxSenha + "'";
+
+            int qt = comb.Runsql();
+                
+            comb.close();
+
+        }
+
+        private void Form1_Resize_1(object sender, EventArgs e)
+         {
+            lblSenha.Width = (sender as Form).Width;
+            lbTexto.Width = (sender as Form).Width;
+            Paciente.Width = (sender as Form).Width;
+            lbHora.Width = (sender as Form).Width;
+
+       }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.ControlBox = false;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lbHora.Text = DateTime.Now.ToString("HH:mm:ss");
+            Paciente.Text = DateTime.Now.ToString("dd/MM/yyyy");
+
+        }
+
+        private void Senha_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
